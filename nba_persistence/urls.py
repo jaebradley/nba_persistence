7fedcb8114898959154e58cd584a1c34bf26c70a
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-27 15:58:09 -0500

    Updated views and urls

--- nba_persistence/urls.py
+++ nba_persistence/urls.py
@@ -17,8 +17,33 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from data.views import TeamViewSet, PositionViewSet, SeasonViewSet, GameViewSet, BoxScoreView, PlayerViewSet
+
 import settings
+from data.views import TeamViewSet, PositionViewSet, SeasonViewSet, GameViewSet, BoxScoreViewSet, PlayerViewSet
+
+team_list = TeamViewSet.as_view({
+    'get': 'list'
+})
+
+team_detail = TeamViewSet.as_view({
+    'get': 'retrieve'
+})
+
+position_list = PositionViewSet.as_view({
+    'get': 'list'
+})
+
+position_detail = PositionViewSet.as_view({
+    'get': 'retrieve'
+})
+
+season_list = SeasonViewSet.as_view({
+    'get': 'list'
+})
+
+season_detail = SeasonViewSet.as_view({
+    'get': 'retrieve'
+})
 
 player_list = PlayerViewSet.as_view({
     'get': 'list',
@@ -27,17 +52,37 @@ player_detail = PlayerViewSet.as_view({
     'get': 'retrieve',
 })
 
+game_list = GameViewSet.as_view({
+    'get': 'list'
+})
+
+game_detail = GameViewSet.as_view({
+    'get': 'retrieve'
+})
+
+box_score_list = BoxScoreViewSet.as_view({
+    'get': 'list'
+})
+
+box_score_detail = BoxScoreViewSet.as_view({
+    'get': 'retrieve'
+})
+
 router = routers.SimpleRouter()
-router.register(r'teams', TeamViewSet)
-router.register(r'seasons', SeasonViewSet)
-router.register(r'games', GameViewSet)
-router.register(r'positions', PositionViewSet)
 
 urlpatterns = [
     url(r'^players/$', player_list, name='player-list'),
     url(r'^players/(?P<pk>[0-9]+)/$', player_detail, name='player-detail'),
-    url(r'^box_scores/$', BoxScoreView.as_view()),
-    url(r'^box_scores/(?P<id>.+)/$', BoxScoreView.as_view()),
+    url(r'^teams/$', team_list, name='team-list'),
+    url(r'^teams/(?P<pk>[0-9]+)/$', team_detail, name='team-detail'),
+    url(r'^positions/$', position_list, name='position-list'),
+    url(r'^positions/(?P<pk>[0-9]+)/$', position_detail, name='position-detail'),
+    url(r'^seasons/$', season_list, name='season-list'),
+    url(r'^seasons/(?P<pk>[0-9]+)/$', season_detail, name='season-detail'),
+    url(r'^games/$', game_list, name='game-list'),
+    url(r'^games/(?P<pk>[0-9]+)/$', game_detail, name='game-detail'),
+    url(r'^box_scores/$', box_score_list, name='box_score-list'),
+    url(r'^box_scores/(?P<pk>[0-9]+)/$', box_score_detail, name='box_score-detail'),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
