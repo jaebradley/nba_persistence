1714131be0fc097c21c3d647298d527f44f97e5e
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-27 03:15:10 -0500

    test

--- nba_persistence/urls.py
+++ nba_persistence/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     url(r'^players/(?P<id>.+)/$', PlayerView.as_view()),
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
