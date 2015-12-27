8705842bc577d75edb77ab5aeb5c922fdf281b1c
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-27 03:03:43 -0500

    updated

--- nba_persistence/settings.py
+++ nba_persistence/settings.py
@@ -121,5 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
