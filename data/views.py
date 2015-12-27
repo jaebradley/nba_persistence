72552e1206ec6efd899168e6520598ea9e4398aa
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-27 11:19:31 -0500

    Updated BoxScore View

--- data/views.py
+++ data/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
+from datetime import datetime
+from pytz import utc
 
 from data.models import Team, Position, Season, Game, Player, BoxScore
 from data.serializers import TeamSerializer, PositionSerializer, SeasonSerializer, GameSerializer, PlayerSerializer, BoxScoreSerializer
@@ -41,21 +43,49 @@ class PlayerView(ListAPIView):
         if 'id' in self.kwargs:
             queryset = queryset.filter(id=self.kwargs['id'])
 
-        if first_name is not None:
-            queryset = queryset.filter(first_name=first_name)
+        else:
+            if first_name is not None:
+                queryset = queryset.filter(first_name=first_name)
 
-        if last_name is not None:
-            queryset = queryset.filter(last_name=last_name)
+            if last_name is not None:
+                queryset = queryset.filter(last_name=last_name)
 
-        if team_abbreviation is not None:
-            queryset = queryset.filter(team__abbreviation=team_abbreviation)
+            if team_abbreviation is not None:
+                queryset = queryset.filter(team__abbreviation=team_abbreviation)
 
-        if position_abbreviation is not None:
-            queryset = queryset.filter(position__abbreviation=position_abbreviation)
+            if position_abbreviation is not None:
+                queryset = queryset.filter(position__abbreviation=position_abbreviation)
 
         return queryset
 
 
 class BoxScoreViewSet(ModelViewSet):
-    queryset = BoxScore.objects.all().order_by('game__start_time').order_by('-player__last_name').order_by('-player__first_name')
     serializer_class = BoxScoreSerializer
+
+    def get_queryset(self):
+        queryset = BoxScore.objects.all().order_by('game__start_time').order_by('-player__last_name').order_by('-player__first_name')
+        first_name = self.request.query_params.get('first_name', None)
+        last_name = self.request.query_params.get('last_name', None)
+        team_abbreviation = self.request.query_params.get('team_abbreviation', None)
+        unix_start_time = self.request.query_params.get('unix_start_time', None)
+        unix_end_time = self.request.query_params.get('unix_end_time', None)
+
+        if 'id' in self.kwargs:
+            queryset = queryset.filter(id=self.kwargs['id'])
+        else:
+            if first_name is not None:
+                queryset = queryset.filter(first_name=first_name)
+
+            if last_name is not None:
+                queryset = queryset.filter(last_name=last_name)
+
+            if team_abbreviation is not None:
+                queryset = queryset.filter(team__abbreviation=team_abbreviation)
+
+            if unix_start_time is not None:
+                queryset = queryset.filter(datetime.fromtimestamp(unix_start_time, utc))
+
+            if unix_end_time is not None:
+                queryset = queryset.filter(datetime.fromtimestamp(unix_end_time, utc))
+
+        return queryset
