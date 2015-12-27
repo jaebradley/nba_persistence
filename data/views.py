bb31583fe50e254e6c52054909555af9f376ebf1
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-27 15:58:50 -0500

    Updated views

--- data/views.py
+++ data/views.py
@@ -1,7 +1,7 @@
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 from datetime import datetime
+
 from pytz import utc
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from data.models import Team, Position, Season, Game, Player, BoxScore
 from data.serializers import TeamSerializer, PositionSerializer, SeasonSerializer, GameSerializer, PlayerSerializer, BoxScoreSerializer
@@ -10,24 +10,60 @@ from data.serializers import TeamSerializer, PositionSerializer, SeasonSerialize
 # Create your views here.
 
 
-class TeamViewSet(ModelViewSet):
-    queryset = Team.objects.all().order_by('name')
+class TeamViewSet(ReadOnlyModelViewSet):
     serializer_class = TeamSerializer
 
+    def get_queryset(self):
+        queryset = Team.objects.all().order_by('name')
+        abbreviation = self.request.query_params.get('abbreviation', None)
+        if abbreviation is not None:
+            queryset = queryset.filter(abbreviation=abbreviation)
 
-class PositionViewSet(ModelViewSet):
-    queryset = Position.objects.all().order_by('name')
+        return queryset
+
+
+class PositionViewSet(ReadOnlyModelViewSet):
     serializer_class = PositionSerializer
 
+    def get_queryset(self):
+        queryset = Position.objects.all().order_by('name')
+        abbreviation = self.request.query_params.get('abbreviation', None)
+        if abbreviation is not None:
+            queryset = queryset.filter(abbreviation=abbreviation)
+        return queryset
 
-class SeasonViewSet(ModelViewSet):
+
+class SeasonViewSet(ReadOnlyModelViewSet):
     queryset = Season.objects.all().order_by('-start_year')
     serializer_class = SeasonSerializer
 
 
-class GameViewSet(ModelViewSet):
-    queryset = Game.objects.all().order_by('start_time')
+class GameViewSet(ReadOnlyModelViewSet):
     serializer_class = GameSerializer
+
+    def get_queryset(self):
+        queryset = Game.objects.all().order_by('start_time')
+        home_team_abbreviation = self.request.query_params.get('home_team_abbreviation', None)
+        away_team_abbreviation = self.request.query_params.get('away_team_abbreviation', None)
+        unix_start_time = self.request.query_params.get('unix_start_time', None)
+        unix_end_time = self.request.query_params.get('unix_end_time', None)
+        season_start_year = self.request.query_params.get('season_start_year', None)
+        if home_team_abbreviation is not None:
+            queryset = queryset.filter(home_team__abbreviation=home_team_abbreviation)
+
+        if away_team_abbreviation is not None:
+            queryset = queryset.filter(away_team__abbreviation=away_team_abbreviation)
+
+        if unix_start_time is not None:
+            queryset = queryset.filter(start_time__gte=datetime.fromtimestamp(unix_start_time, utc))
+
+        if unix_end_time is not None:
+            queryset = queryset.filter(start_time__lte=datetime.fromtimestamp(unix_end_time, utc))
+
+        if season_start_year is not None:
+            queryset = queryset.filter(season__start_year=season_start_year)
+
+        return queryset
 
 
 class PlayerViewSet(ReadOnlyModelViewSet):
@@ -54,7 +90,7 @@ class PlayerViewSet(ReadOnlyModelViewSet):
         return queryset
 
 
-class BoxScoreView(ListAPIView):
+class BoxScoreViewSet(ReadOnlyModelViewSet):
     serializer_class = BoxScoreSerializer
 
     def get_queryset(self):
@@ -65,22 +101,19 @@ class BoxScoreView(ListAPIView):
         unix_start_time = self.request.query_params.get('unix_start_time', None)
         unix_end_time = self.request.query_params.get('unix_end_time', None)
 
-        if 'id' in self.kwargs:
-            queryset = queryset.filter(id=self.kwargs['id'])
-        else:
-            if first_name is not None:
-                queryset = queryset.filter(first_name=first_name)
+        if first_name is not None:
+            queryset = queryset.filter(first_name=first_name)
 
-            if last_name is not None:
-                queryset = queryset.filter(last_name=last_name)
+        if last_name is not None:
+            queryset = queryset.filter(last_name=last_name)
 
-            if team_abbreviation is not None:
-                queryset = queryset.filter(team__abbreviation=team_abbreviation)
+        if team_abbreviation is not None:
+            queryset = queryset.filter(team__abbreviation=team_abbreviation)
 
-            if unix_start_time is not None:
-                queryset = queryset.filter(datetime.fromtimestamp(unix_start_time, utc))
+        if unix_start_time is not None:
+            queryset = queryset.filter(datetime.fromtimestamp(unix_start_time, utc))
 
-            if unix_end_time is not None:
-                queryset = queryset.filter(datetime.fromtimestamp(unix_end_time, utc))
+        if unix_end_time is not None:
+            queryset = queryset.filter(datetime.fromtimestamp(unix_end_time, utc))
 
         return queryset
