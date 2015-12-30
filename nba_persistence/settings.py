0aaeb1b8c39c0167a82828286831da76af67f6b3
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-29 23:10:09 -0500

    Heroku bs fix

--- nba_persistence/settings.py
+++ nba_persistence/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 DATABASES = { 'default' : dj_database_url.config()}
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
