f33a5cddd4ed3199f819569513f7f8cdb3b40e42
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-30 00:03:25 -0500

    test

--- data/inserters/utils.py
+++ data/inserters/utils.py
@@ -2,11 +2,17 @@ def draftkings_salary_team_abbreviation_converter(draftkings_team_abbreviation):
     if draftkings_team_abbreviation.upper() == "NY":
         return "NYK"
 
+    if draftkings_team_abbreviation.upper() == 'CHA':
+        return 'CHO'
+
     return draftkings_team_abbreviation.upper()
 
 
 def fanduel_salary_team_abbreviation_converter(fanduel_team_abbreviation):
     if fanduel_team_abbreviation.upper() == "GS":
         return "GSW"
+
+    if fanduel_team_abbreviation.upper() == 'CHA':
+        return 'CHO'
 
     return fanduel_team_abbreviation.upper()
