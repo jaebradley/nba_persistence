450972fa55466ced05d4ec51146798871248a1a9
Jae Bradley <jae.b.bradley@gmail.com>
2015-12-30 00:09:53 -0500

    test

--- data/inserters/utils.py
+++ data/inserters/utils.py
@@ -8,6 +8,9 @@ def draftkings_salary_team_abbreviation_converter(draftkings_team_abbreviation):
     if draftkings_team_abbreviation.upper() == 'SA':
         return 'SAS'
 
+    if draftkings_team_abbreviation.upper() == 'BKN':
+        return 'BRK'
+
     return draftkings_team_abbreviation.upper()
 
 
@@ -20,5 +23,8 @@ def fanduel_salary_team_abbreviation_converter(fanduel_team_abbreviation):
 
     if fanduel_team_abbreviation.upper() == 'SA':
         return 'SAS'
+
+    if fanduel_team_abbreviation.upper() == 'BKN':
+        return 'BRK'
 
     return fanduel_team_abbreviation.upper()
