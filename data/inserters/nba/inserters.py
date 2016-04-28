ff49d4408a940db9abc519aee0a9c9212705b091
jaebradley <jae.b.bradley@gmail.com>
2016-04-28 01:39:25 -0700

    updated inserters

--- data/inserters/nba/inserters.py
+++ data/inserters/nba/inserters.py
@@ -4,27 +4,26 @@ from data.models import BoxScore
 
 
 def insert_box_score(box_score):
-    translated_box_score = nba_translators.translate_box_score(box_score=box_score)
     BoxScore.objects.update_or_create(
-            player=translated_box_score.player,
-            game=translated_box_score.game,
-            seconds_played=translated_box_score.seconds_played,
-            field_goals=translated_box_score.field_goals,
-            field_goal_attempts=translated_box_score.field_goal_attempts,
-            three_point_field_goals=translated_box_score.three_point_field_goals,
-            three_point_field_goal_attempts=translated_box_score.three_point_field_goal_attempts,
-            free_throws=translated_box_score.free_throws,
-            free_throw_attempts=translated_box_score.free_throw_attempts,
-            offensive_rebounds=translated_box_score.offensive_rebounds,
-            defensive_rebounds=translated_box_score.defensive_rebounds,
-            total_rebounds=translated_box_score.total_rebounds,
-            assists=translated_box_score.assists,
-            steals=translated_box_score.steals,
-            blocks=translated_box_score.blocks,
-            turnovers=translated_box_score.turnovers,
-            fouls_committed=translated_box_score.personal_fouls,
-            points=translated_box_score.points,
-            draftkings_points=translated_box_score.draftkings_points
+            player=box_score.player,
+            game=box_score.game,
+            seconds_played=box_score.seconds_played,
+            field_goals=box_score.field_goals,
+            field_goal_attempts=box_score.field_goal_attempts,
+            three_point_field_goals=box_score.three_point_field_goals,
+            three_point_field_goal_attempts=box_score.three_point_field_goal_attempts,
+            free_throws=box_score.free_throws,
+            free_throw_attempts=box_score.free_throw_attempts,
+            offensive_rebounds=box_score.offensive_rebounds,
+            defensive_rebounds=box_score.defensive_rebounds,
+            total_rebounds=box_score.total_rebounds,
+            assists=box_score.assists,
+            steals=box_score.steals,
+            blocks=box_score.blocks,
+            turnovers=box_score.turnovers,
+            fouls_committed=box_score.personal_fouls,
+            points=box_score.points,
+            draftkings_points=box_score.draftkings_points
         )
 
 
