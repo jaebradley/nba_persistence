b064d5438263138ac06e92f981bebca7ed259773
jaebradley <jae.b.bradley@gmail.com>
2016-08-06 01:38:19 -0400

    move positions to own

---
+++ data/positions.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Position(Enum):
+    point_guard = 'Point Guard',
+    shooting_guard = 'Shooting Guard',
+    small_forward = 'Small Forward',
+    power_forward = 'Power Forward',
+    center = 'Center'
